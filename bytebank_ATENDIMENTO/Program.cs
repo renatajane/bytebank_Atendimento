b8e42378ea26142d10e6ab0b317436f58e53c051
renatajane <renatajane28@gmail.com>
2023-05-17 20:20:26 -0300

    first commit

---
+++ bytebank_ATENDIMENTO/Program.cs
@@ -0,0 +1,35 @@
+﻿using bytebank.Modelos.ADM.Funcionarios;
+using bytebank_ATENDIMENTO.bytebank.Atendimento;
+using bytebank_GeradorChavePix;
+using bytebank_Modelos.bytebank.Modelos.ADM.Utilitario;
+using System.Linq.Expressions;
+
+Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+//new ByteBankAtendimento().AtendimentoCliente();
+
+Console.WriteLine(GeradorPix.GetChavePix());
+
+var listaDeChaves = GeradorPix.GetChavesPix(10); 
+
+foreach (var chave in listaDeChaves)
+{
+    Console.WriteLine(chave);
+}
+
+
+//public class Estagiario : Funcionario
+//{
+//    public Estagiario(double salario, string cpf) : base(salario, cpf)
+//    {
+//    }
+
+//    public override void AumentarSalario()
+//    {
+//        throw new NotImplementedException();
+//    }
+
+//    protected override double getBonificacao()
+//    {
+//        throw new NotImplementedException();
+//    }
+//}
---
+++ bytebank_Modelos/bytebank.Modelos/ADM/ParceriaComercial/ParceiroComercial.cs
@@ -0,0 +1,17 @@
+﻿using bytebank.Modelos.ADM.SistemaInterno;
+using bytebank_Modelos.bytebank.Modelos.ADM.Utilitario;
+
+namespace bytebank.Modelos.ADM.Utilitario
+{
+    public class ParceiroComercial : IAutenticavel
+    {
+        public string Senha { get; set; }
+        public AutenticacaoUtil Autenticador { get; set; }
+
+
+        public bool Autenticar(string senha)
+        {
+            return this.Autenticador.ValidarSenha(this.Senha, senha);
+        }
+    }
+}
